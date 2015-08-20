e74ad6ccb682aa603b8d007547473e4b609f8036
Ian Kinsey <ian.kinsey@gmail.com>
2015-08-19 22:04:25 -0700

    Added C to the outputted text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello from Cisco Shipped! B');
+    res.send('Hello from Cisco Shipped! C');
 });
 
 var server = app.listen(3000, function () {
